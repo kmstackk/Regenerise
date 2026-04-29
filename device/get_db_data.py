248dd867ca297f2057c06cbb91e292ae6909f098
boyer-cs <boyercalum@gmail.com>
2026-04-29 19:15:34 +0100

    Working version get_db_data.py

--- device/get_db_data.py
+++ device/get_db_data.py
@@ -9,7 +9,6 @@ USERNAME = "group05@cardiff.ac.uk"
 PASSWORD = "group052026"
 DEVICE_ID = "c6f095a0-2df1-11f1-81d7-dd37020598c0"
 
-
 def get_token():
     res = requests.post(
         f"{BASE_URL}/api/auth/login",
@@ -18,25 +17,23 @@ def get_token():
     res.raise_for_status()
     return res.json()["token"]
 
-
 def get_headers():
     token = get_token()
     return {"X-Authorization": f"Bearer {token}"}
 
-
-# get alarms
+# Get alarms
 headers = get_headers()
 res = requests.get(
     f"{BASE_URL}/api/plugins/telemetry/DEVICE/{DEVICE_ID}/values/attributes/SHARED_SCOPE?keys=alarms",
     headers=headers
 )
-
 data = res.json()
 
-# extract the alarms list
+# Extract the alarms list
 alarms = next((item["value"] for item in data if item["key"] == "alarms"), [])
 
-
 for alarm in alarms:
-    if alarm == curr_time:
-        print("True") # tell node red to beep the alarm
+    time = alarm[0]
+    enabled = alarm[1]
+    if enabled and time == curr_time:
+        print("True")  # Tell Node-RED to trigger the buzzer
