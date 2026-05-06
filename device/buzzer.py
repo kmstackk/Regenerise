f78928ac7e89d7da73044548df47758d4c064242
boyer-cs <boyercalum@gmail.com>
2026-05-06 10:27:50 +0100

    Update buzzer.py

--- device/buzzer.py
+++ device/buzzer.py
@@ -1,11 +1,11 @@
 from grovepi import *
 import time
 
-buzzer = 2 
-
+buzzer = 2  # D2
+pinMode(buzzer, "OUTPUT")
 
 while True:
-	digitalWrite(buzzer, 1)
-	time.sleep(0.5)
-	digitalWrite(buzzer,0)
-	time.sleep(0.5)
+    digitalWrite(buzzer, 1)  # On
+    time.sleep(0.5)
+    digitalWrite(buzzer, 0)  # Off
+    time.sleep(0.5)
